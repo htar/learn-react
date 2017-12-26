d263da1fa3aebae301ae904d6ee1a94461d3d790
Taras <khmelovski@gmail.com>
2017-12-27 00:50:44 +0200

    add form

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
+import UserForm from './component/UserForm';
 import ArticleList from './component/ArticleList';
 import articles from './fixtures';
 class App extends Component {
   render() {
-    return (
-      <div className="App">
+    return <div className="App">
         <div className="container">
           <div className="row">
-            <div className="col-xs-12">
+            <div className="col-xs-12 text-center">
+             < UserForm />
               <ArticleList articles={articles} />
             </div>
           </div>
         </div>
-      </div>
-    );
+      </div>;
   }
 }
 
